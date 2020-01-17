344a3ad850df444bf43ea21cc493dd5ecd2a1d14
ibkon <btinggo@gmail.com>
2020-01-18 01:03:11 +0800

    Initialize the project

---
+++ src/main/java/top/yukino/shpee/ShpeeAppliction.java
@@ -0,0 +1,13 @@
+package top.yukino.shpee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShpeeAppliction {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShpeeAppliction.class, args);
+	}
+
+}
