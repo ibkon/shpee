6d3bd002bb8ed5dea457ec2d030ec5774ae254d0
ibkon <btinggo@gmail.com>
2020-03-25 01:25:13 +0800

    Update

--- src/main/java/top/yukino/shpee/control/Index.java
+++ src/main/java/top/yukino/shpee/control/Index.java
@@ -19,20 +19,52 @@ public class Index extends Super {
 	public String indexPage(HttpServletRequest request, Map<String, Object> map) {
 
 		List<Map<String, Object>> lMaps = mapper.select(
-				"SELECT path,hash,type FROM T_UPLOAD WHERE isdelete=0 AND (type='png' OR type='jpg' OR type='jpeg' OR type='gif') AND size<0x100000");
+				"SELECT path,hash,type FROM T_UPLOAD WHERE isdelete=0 AND (type='png' OR type='jpg' OR type='jpeg' OR type='gif') AND size<0x200000");
 		List<String> paths = new ArrayList<String>();
 		Map<String, Object> mbuffer = new HashMap<String, Object>();
 		Random random = new Random();
 		String path, hash, type;
-		for (int i = 0; i < (3 > lMaps.size() ? lMaps.size() : 3); i++) {
-			mbuffer = lMaps.get(random.nextInt(lMaps.size()));
-			if(mbuffer==null)
-				break;
-			path = mbuffer.get("PATH").toString();
-			hash = mbuffer.get("HASH").toString();
-			type = mbuffer.get("TYPE").toString();
-			paths.add(path.replaceAll("upload", "static")+"/"+DigestUtils.md5Hex(hash).toUpperCase()+"."+type);
-			lMaps.remove(mbuffer);
+		if (lMaps.size() > 3) {
+			int count[] = new int[3];
+			count[0] = random.nextInt(lMaps.size());
+			count[1] = random.nextInt(lMaps.size());
+			count[2] = random.nextInt(lMaps.size());
+			while (true) {
+				if (count[0] == count[1]) {
+					count[1] = random.nextInt(lMaps.size());
+					continue;
+				}
+				if (count[0] == count[2]) {
+					count[2] = random.nextInt(lMaps.size());
+					continue;
+				}
+				if (count[1] == count[2]) {
+					count[2] = random.nextInt(lMaps.size());
+					continue;
+				}
+
+				if ((count[0] != count[1]) && (count[0] != count[2]) && (count[1] != count[2]))
+					break;
+			}
+			for (int i : count) {
+				mbuffer = lMaps.get(i);
+				path = mbuffer.get("PATH").toString();
+				hash = mbuffer.get("HASH").toString();
+				type = mbuffer.get("TYPE").toString();
+				paths.add(path.replaceAll("upload", "static") + "/" + DigestUtils.md5Hex(hash).toUpperCase() + "."
+						+ type);
+
+			}
+		} else {
+			for (int i = 0; i < lMaps.size(); i++) {
+				mbuffer = lMaps.get(i);
+				path = mbuffer.get("PATH").toString();
+				hash = mbuffer.get("HASH").toString();
+				type = mbuffer.get("TYPE").toString();
+				paths.add(path.replaceAll("upload", "static") + "/" + DigestUtils.md5Hex(hash).toUpperCase() + "."
+						+ type);
+
+			}
 		}
 
 		map.put("imageList", paths);
