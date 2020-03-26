44420e975d5a053676977a319a23bfa2e38c0d53
ibkon <btinggo@gmail.com>
2020-03-26 21:11:04 +0800

    Update random value of homepage random picture.

--- src/main/java/top/yukino/shpee/control/Index.java
+++ src/main/java/top/yukino/shpee/control/Index.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class Index extends Super {
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String indexPage(HttpServletRequest request, Map<String, Object> map) {
 
@@ -22,51 +25,36 @@ public class Index extends Super {
 				"SELECT path,hash,type FROM T_UPLOAD WHERE isdelete=0 AND (type='png' OR type='jpg' OR type='jpeg' OR type='gif') AND size<0x200000");
 		List<String> paths = new ArrayList<String>();
 		Map<String, Object> mbuffer = new HashMap<String, Object>();
-		Random random = new Random();
+		Set<Integer> 	number	= new TreeSet<Integer>();
+		Random random = new Random(System.currentTimeMillis());
 		String path, hash, type;
-		if (lMaps.size() > 3) {
-			int count[] = new int[3];
-			count[0] = random.nextInt(lMaps.size());
-			count[1] = random.nextInt(lMaps.size());
-			count[2] = random.nextInt(lMaps.size());
-			while (true) {
-				if (count[0] == count[1]) {
-					count[1] = random.nextInt(lMaps.size());
-					continue;
-				}
-				if (count[0] == count[2]) {
-					count[2] = random.nextInt(lMaps.size());
-					continue;
-				}
-				if (count[1] == count[2]) {
-					count[2] = random.nextInt(lMaps.size());
-					continue;
-				}
-
-				if ((count[0] != count[1]) && (count[0] != count[2]) && (count[1] != count[2]))
-					break;
-			}
-			for (int i : count) {
-				mbuffer = lMaps.get(i);
-				path = mbuffer.get("PATH").toString();
-				hash = mbuffer.get("HASH").toString();
-				type = mbuffer.get("TYPE").toString();
+		if(lMaps.size()<=3) {
+			for(Map<String, Object> m:lMaps) {
+				path = m.get("PATH").toString();
+				hash = m.get("HASH").toString();
+				type = m.get("TYPE").toString();
 				paths.add(path.replaceAll("upload", "static") + "/" + DigestUtils.md5Hex(hash).toUpperCase() + "."
 						+ type);
-
-			}
-		} else {
-			for (int i = 0; i < lMaps.size(); i++) {
-				mbuffer = lMaps.get(i);
-				path = mbuffer.get("PATH").toString();
-				hash = mbuffer.get("HASH").toString();
-				type = mbuffer.get("TYPE").toString();
-				paths.add(path.replaceAll("upload", "static") + "/" + DigestUtils.md5Hex(hash).toUpperCase() + "."
-						+ type);
-
 			}
 		}
-
+		else {
+			//循环10次，生成三个不重复随机数，但不保证必须够3个
+			int thisNumber	= 0;
+			for(int i=0;i<10;i++) {
+				thisNumber=random.nextInt((int)System.currentTimeMillis())%lMaps.size();
+				if(number.add(thisNumber))
+				{
+					mbuffer = lMaps.get(thisNumber);
+					path = mbuffer.get("PATH").toString();
+					hash = mbuffer.get("HASH").toString();
+					type = mbuffer.get("TYPE").toString();
+					paths.add(path.replaceAll("upload", "static") + "/" + DigestUtils.md5Hex(hash).toUpperCase() + "."
+							+ type);
+				}
+				if(number.size()==3)
+					break;
+			}
+		}
 		map.put("imageList", paths);
 		if (request.getHeader("User-Agent").contains("iPhone") || request.getHeader("User-Agent").contains("Android")) {
 			map.put("device", "mobile");
