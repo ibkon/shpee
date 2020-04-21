979e6bdcd520c5837600099d2176d38aa63177b3
ibkon <btinggo@gmail.com>
2020-04-21 23:49:32 +0800

    Remove redundant code.

--- src/main/java/top/yukino/shpee/ShpeeApplication.java
+++ src/main/java/top/yukino/shpee/ShpeeApplication.java
@@ -4,15 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.*;
-import java.util.*;
-
 @MapperScan(value = "top.yukino.shpee.conf")
 @SpringBootApplication
-public class ShpeeAppliction {
+public class ShpeeApplication {
 
-	public static void main(String[] args) throws IOException {
-		SpringApplication.run(ShpeeAppliction.class, args);
+	public static void main(String[] args) {
+		SpringApplication.run(ShpeeApplication.class, args);
 	}
 
 }
