4c86484a6907a6cd7102869e3542f2d9df454b55
ibkon <btinggo@gmail.com>
2020-12-21 01:11:49 +0800

    update docx tool.

--- src/main/java/com/xentn/shpee/base/BuildDocx.java
+++ src/main/java/com/xentn/shpee/base/BuildDocx.java
@@ -1,35 +1,67 @@
 package com.xentn.shpee.base;
 
+import net.coobird.thumbnailator.Thumbnails;
+import org.apache.commons.io.IOUtils;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.util.Units;
 import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
 
+import java.awt.image.BufferedImage;
+import java.io.*;
+
 /***
  * 文档生成
  */
 public class BuildDocx {
-
-    private XWPFDocument document;
-    XWPFParagraph   paragraph;
-
-    public  BuildDocx(){
+    XWPFDocument    document;
+    OutputStream    outputStream;
+    public BuildDocx(){
         this(null);
     }
-    public  BuildDocx(String outFile){
+    public BuildDocx(OutputStream outputStream){
+        this.outputStream   = outputStream;
         document = new XWPFDocument();
-        paragraph = document.createParagraph();
     }
-    //添加节点
-    public void addNote(String text){
+    //添加标题
+    public void addTest(String test){
+        XWPFParagraph paragraph = document.createParagraph();
+        paragraph.setAlignment(ParagraphAlignment.LEFT);
         XWPFRun run = paragraph.createRun();
-        run.setText(text);
-        run.setFontSize(11);
+        run.setText(test);
+        run.setColor("000000");
+        run.setFontSize(20);
     }
     //添加图片
-    public void addImage(String images){
-        XWPFRun run = paragraph.createRun();
+    public void addImage(String path,String fileName,int type) {
+        XWPFParagraph   paragraph   = document.createParagraph();
+        XWPFRun         run         = paragraph.createRun();
+        InputStream     inputStream = null;
+        BufferedImage   image       = null;
+        byte[]          bs          = null;
+
         paragraph.setAlignment(ParagraphAlignment.CENTER);
 
+        try {
+            inputStream = new FileInputStream(path);
+            image = Thumbnails.of(path).size(500,500).asBufferedImage();
+            bs = IOUtils.toByteArray(inputStream);
+            run.addPicture(new ByteArrayInputStream(bs), type,"", Units.toEMU(image.getWidth()),Units.toEMU(image.getHeight()));
+            inputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidFormatException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void flush() throws IOException {
+        document.write(new FileOutputStream("test.docx"));
+        document.close();
     }
 }
